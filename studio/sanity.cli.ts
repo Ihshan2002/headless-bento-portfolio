f4ba3eeda3ed095496f87c2f74cacfc94bed3c76
Asd@1234 <super-dev@xenbird.com>
2026-02-16 15:24:49 +0530

    bug fix

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -6,10 +6,6 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
-    // This ID connects your local code to the Sanity project
-    appId: 'k8lbv5dd', 
-    autoUpdates: true,
-  },
-  // This defines your URL: https://ihsan-bento.sanity.studio
-  studioHost: 'ihsan-bento' 
+    appId: 'lpj8wrlcrj4xba68talob4m3',
+  }
 })
