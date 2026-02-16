f1f7d4692e57a6e04b2f83e401cbcb11635a3fd3
Asd@1234 <super-dev@xenbird.com>
2026-02-16 11:21:37 +0530

    set root directory to frontend and update sanity cli config

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -6,10 +6,10 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-     */
+    // This ID connects your local code to the Sanity project
+    appId: 'k8lbv5dd', 
     autoUpdates: true,
-  }
+  },
+  // This defines your URL: https://ihsan-bento.sanity.studio
+  studioHost: 'ihsan-bento' 
 })
